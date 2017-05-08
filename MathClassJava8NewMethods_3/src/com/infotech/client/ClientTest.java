e4fa80f810a603d5c3a32fdd446c49a53bcc23d4
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-05-08 09:54:29 +0530

    MathClassJava8NewMethods_3 Source code

---
+++ MathClassJava8NewMethods_3/src/com/infotech/client/ClientTest.java
@@ -0,0 +1,24 @@
+package com.infotech.client;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		
+		double nextDown = Math.nextDown(Double.NaN);
+		System.out.println(nextDown);
+		
+		double nextDown2 = Math.nextDown(Double.NEGATIVE_INFINITY);
+		System.out.println(nextDown2);
+		
+		double nextDown3 = Math.nextDown(0);
+		System.out.println(nextDown3);
+		
+		double nextDown4 = Math.nextDown(2);
+		System.out.println(nextDown4);
+		
+		double nextDown5 = Math.nextDown(-2);
+		System.out.println(nextDown5);
+		
+	}
+
+}
